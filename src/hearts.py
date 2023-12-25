1a1b3f0d85f4c99accf8883fd035ca4730d6f6d0
Serhii_R <rsp89@i.ua>
2023-12-25 15:55:30 +0200

    UPDATED: hearts.py

--- src/hearts.py
+++ src/hearts.py
@@ -1,10 +1,17 @@
 import random
+from typing import Tuple, List, Sequence, TypeVar, Optional
 
 SUITS = "♠ ♡ ♢ ♣".split()
 RANKS = "2 3 4 5 6 7 8 9 10 J Q K A".split()
 
+Card = Tuple[str, str]  # tuple[str, str]
+Deck = List[Card]  # list[Card]
 
-def get_deck(shuffle=False):
+
+Choosable = TypeVar("Choosable")
+
+
+def get_deck(shuffle: bool = False) -> Deck:
     """Get a new deck of 52 cards."""
     deck = [(s, r) for r in RANKS for s in SUITS]
     if shuffle:
@@ -12,20 +19,38 @@ def get_deck(shuffle=False):
     return deck
 
 
-def deal_hands(deck):
+def choose(items: Sequence[Choosable]) -> Choosable:
+    """Choose and return a random item."""
+    return random.choice(items)
+
+
+def player_order(names: List[str], start: Optional[str] = None) -> List[str]:
+    """Rotate player order so that start goes first."""
+    if start is None:
+        start = choose(names)
+    start_idx = names.index(start)
+    return names[start_idx:] + names[:start_idx]
+
+
+def deal_hands(deck: Deck) -> Tuple[Deck, Deck, Deck, Deck]:
     """Deal the cards in the deck into four hands"""
     return deck[0::4], deck[1::4], deck[2::4], deck[3::4]
 
 
-def play():
+def play() -> None:
     """Play a 4-player card game"""
     deck = get_deck(shuffle=True)
     names = "P1 P2 P3 P4".split()
-    hands = {n: h for n, h in zip(names, deal_hands(deck))}
+    hands: dict[str, Deck] = {n: h for n, h in zip(names, deal_hands(deck))}
+    start_player = choose(names)
+    turn_order = player_order(names, start=start_player)
 
-    for name, cards in hands.items():
-        card_str = " ".join(f"{s}{r}" for (s, r) in cards)
-        print(f"{name}: {card_str}")
+    while hands[start_player]:
+        for name in turn_order:
+            card = choose(hands[name])
+            hands[name].remove(card)
+            print(f"{name}: {card[0] + card[1]:<3} ", end="")
+        print()
 
 
 if __name__ == "__main__":
